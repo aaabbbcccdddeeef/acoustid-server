1996fb3aff86a6fba8ffc115b54bb65ab2b99d61
Lukáš Lalinský <lalinsky@gmail.com>
2019-09-13 14:17:40 +0200

    Fix GitLab CI build

--- admin/ci/run-tests.sh
+++ admin/ci/run-tests.sh
@@ -4,8 +4,8 @@ set -ex
 
 /wait-for-it.sh $ACOUSTID_TEST_INDEX_HOST:6080
 /wait-for-it.sh $ACOUSTID_TEST_REDIS_HOST:6379
-/wait-for-it.sh $ACOUSTID_TEST_DB_DEFAULT_HOST:5432
-/wait-for-it.sh $ACOUSTID_TEST_DB_SLOW_HOST:5432
+/wait-for-it.sh $ACOUSTID_TEST_DB_MAIN_HOST:5432
+/wait-for-it.sh $ACOUSTID_TEST_DB_IMPORT_HOST:5432
 
 export PIP_CACHE_DIR=`pwd`/pip-cache
 
